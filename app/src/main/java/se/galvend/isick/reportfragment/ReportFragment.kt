26297e9b4b1d192efe5a85706632cf3b8b4ca7fa
GalvenD <dennis.galven@gmail.com>
2018-02-12 15:22:59 +0100

    -added logotype -started working with layouts

--- app/src/main/java/se/galvend/isick/reportfragment/ReportFragment.kt
+++ app/src/main/java/se/galvend/isick/reportfragment/ReportFragment.kt
@@ -3,15 +3,30 @@ package se.galvend.isick.reportfragment
 
 import android.os.Bundle
 import android.support.v4.app.Fragment
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import kotlinx.android.synthetic.main.fragment_report.*
 
 import se.galvend.isick.R
 
 class ReportFragment : Fragment() {
+    companion object {
+        val TAG = "ReportFragment"
+    }
     override fun onCreateView(inflater: LayoutInflater?, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         return inflater!!.inflate(R.layout.fragment_report, container, false)
+    }
+
+    override fun onViewCreated(view: View?, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        nameLabel.text = "Dennis Galvén"
+
+        vabSwitch.setOnCheckedChangeListener { _, _ ->
+            Log.d(TAG, vabSwitch.isChecked.toString())
+        }
+
     }
 
 }// Required empty public constructor
