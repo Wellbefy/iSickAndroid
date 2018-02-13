d52a854cdac0af4e96a6986eb0f1ae9e3579241a
GalvenD <dennis.galven@gmail.com>
2018-02-13 13:25:22 +0100

    -added circle progress to history vc -continued work on layout

--- app/src/main/java/se/galvend/isick/historyfragment/HistoryFragment.kt
+++ app/src/main/java/se/galvend/isick/historyfragment/HistoryFragment.kt
@@ -6,6 +6,7 @@ import android.support.v4.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import kotlinx.android.synthetic.main.fragment_history.*
 
 import se.galvend.isick.R
 
@@ -19,4 +20,8 @@ class HistoryFragment : Fragment() {
         return inflater!!.inflate(R.layout.fragment_history, container, false)
     }
 
+    override fun onViewCreated(view: View?, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+    }
 }// Required empty public constructor
